9b5cd5edb78b476c88da1ae00d12131b289584de
Patrick <manserpa@student.ethz.ch>
2017-03-02 11:06:26 +0100

    playground manserpa

---
+++ playgrounds/ivtExt/src/main/java/playground/manserpa/minibus/runMinibus.java
@@ -0,0 +1,43 @@
+package playground.manserpa.minibus;
+
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.minibus.hook.PModule;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+
+/**
+ * Entry point, registers all necessary hooks
+ * 
+ * @author aneumann
+ */
+public final class RunMinibus {
+
+	private final static Logger log = Logger.getLogger(RunMinibus.class);
+
+	public static void main(final String[] args) {
+
+		if(args.length == 0){
+			log.info("Arg 1: config.xml is missing.");
+			log.info("Check http://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/atlantis/minibus/ for an example.");
+			System.exit(1);
+		}
+
+		Config config = ConfigUtils.loadConfig( args[0], new PConfigGroup() ) ;
+
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+
+		Controler controler = new Controler(scenario);
+		controler.getConfig().controler().setCreateGraphs(false);
+
+		controler.addOverridingModule(new PModule()) ;
+
+
+		controler.run();
+	}		
+}
