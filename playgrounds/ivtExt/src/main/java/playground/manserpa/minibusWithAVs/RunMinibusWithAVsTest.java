097ac7a2ae36a563b435a2cc129049a573e2b94c
Patrick <manserpa@student.ethz.ch>
2017-04-17 10:14:19 +0200

    Running files

--- playgrounds/ivtExt/src/main/java/playground/manserpa/minibusWithAVs/RunMinibusWithAVsTest.java
+++ playgrounds/ivtExt/src/main/java/playground/manserpa/minibusWithAVs/RunMinibusWithAVsTest.java
@@ -1,4 +1,4 @@
-package playground.manserpa.minibus;
+package playground.manserpa.minibusWithAVs;
 
 
 import org.apache.log4j.Logger;
@@ -9,7 +9,6 @@ import org.matsim.contrib.dynagent.run.DynQSimModule;
 import org.matsim.contrib.minibus.PConfigGroup;
 import org.matsim.contrib.minibus.hook.PModule;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -22,27 +21,36 @@ import playground.sebhoerl.avtaxi.framework.AVQSimProvider;
 /**
  * Entry point, registers all necessary hooks
  * 
- * @author aneumann
+ * @author manserpa
  */
-public final class RunMinibusTest {
+public final class RunMinibusWithAVsTest {
 
-	private final static Logger log = Logger.getLogger(RunMinibusTest.class);
+	private final static Logger log = Logger.getLogger(RunMinibusWithAVsTest.class);
 
 	public static void main(final String[] args) {
 		
 		String configFile = args[0];
 		
-		Config config = ConfigUtils.loadConfig( configFile, new PConfigGroup() ) ;
 		
+		DvrpConfigGroup dvrpConfigGroup = new DvrpConfigGroup();
+		dvrpConfigGroup.setTravelTimeEstimationAlpha(0.05);
+
+		Config config = ConfigUtils.loadConfig( configFile, new PConfigGroup(), new AVConfigGroup(), dvrpConfigGroup ) ;
+		
+		//Config config = ConfigUtils.loadConfig( configFile, new PConfigGroup() ) ;
+
 		Scenario scenario = ScenarioUtils.loadScenario(config);
+
 
 		Controler controler = new Controler(scenario);
 		controler.getConfig().controler().setCreateGraphs(false);
+		controler.addOverridingModule(VrpTravelTimeModules.createTravelTimeEstimatorModule());
+		controler.addOverridingModule(new DynQSimModule<>(AVQSimProvider.class));
+		controler.addOverridingModule(new AVModule());
 
 		controler.addOverridingModule(new PModule()) ;
-		
-		//ConfigUtils.addOrGetModule(scenario.getConfig(), PConfigGroup.class ).setUseAVContrib(true);
-		
+
+
 		controler.run();
 		
 	}		
---
+++ playgrounds/ivtExt/src/main/java/playground/manserpa/minibusWithAVs/RunMinibusWithAVsZurich.java
@@ -0,0 +1,5 @@
+package playground.manserpa.minibusWithAVs;
+
+public class RunMinibusWithAVsZurich {
+//TODO
+}
