347592e83f4257ae93f52a80be9a5bad0002768c
Sebastian Hörl <hoerl.sebastian@gmail.com>
2017-04-28 16:50:40 +0200

    probably fixed AV travel times

--- playgrounds/ivtExt/src/main/java/playground/sebhoerl/recharging_avs/RunTestSimulation.java
+++ playgrounds/ivtExt/src/main/java/playground/sebhoerl/recharging_avs/RunTestSimulation.java
@@ -1,11 +1,13 @@
 package playground.sebhoerl.recharging_avs;
 
+import com.google.inject.Key;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.trafficmonitoring.VrpTravelTimeModules;
 import org.matsim.contrib.dynagent.run.DynQSimModule;
 import org.matsim.core.config.Config;
@@ -26,9 +28,11 @@ import playground.sebhoerl.recharging_avs.calculators.StaticChargeCalculatorModu
 
 import java.net.MalformedURLException;
 
-public class RunSimulation {
+public class RunTestSimulation {
     public static void main(String[] args) throws MalformedURLException {
-        String configFile = args[0];
+        String configFile = "/home/sebastian/belser/stage2/config_opt_v1_8000.xml";
+        String baselinePlansPath = "/home/sebastian/belser/testing/output_plans.xml.gz";
+        String outputPath = "/home/sebastian/belser/testing/output";
 
         DvrpConfigGroup dvrpConfigGroup = new DvrpConfigGroup();
         dvrpConfigGroup.setTravelTimeEstimationAlpha(0.05);
@@ -37,6 +41,11 @@ public class RunSimulation {
                 configFile,
                 new AVConfigGroup(), dvrpConfigGroup, new RechargingConfig(),
                 new StaticChargeCalculatorConfig(), new BinnedChargeCalculatorConfig());
+
+        config.controler().setOutputDirectory(outputPath);
+        config.plans().setInputFile(baselinePlansPath);
+        config.controler().setLastIteration(10);
+        config.controler().setWriteEventsInterval(10);
 
         Scenario scenario = ScenarioUtils.createScenario(config);
         scenario.getPopulation().getFactory().getRouteFactories().setRouteFactory(AVRoute.class, new AVRouteFactory());
